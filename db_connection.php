97ff0ee9d5176c7ee6d18f3a4d163f49215184b1
Quan <21013237@st.phenikaa-uni.edu.vn>
2023-07-18 21:31:27 +0700

    a

--- db_connection.php
+++ db_connection.php
@@ -1,9 +1,9 @@
 <?php
         // Kết nối đến cơ sở dữ liệu
-        $servername = "localhost:3307";
+        $servername = "localhost:3306";
         $username = "root";
         $password = "";
-        $dbname = "movie_db";
+        $dbname = "bruh";
 
         $connection = new mysqli($servername, $username, $password, $dbname);
 
@@ -11,3 +11,4 @@
         if ($connection->connect_error) {
             die("Không thể kết nối đến cơ sở dữ liệu: " . $conn->connect_error);
         }
+?>
